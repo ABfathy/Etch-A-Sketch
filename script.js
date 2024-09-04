b2c38883da0cc9e47c7f6afcc5807a624c4fbd88
ABfathy <113472069+ABfathy@users.noreply.github.com>
2024-09-04 19:34:40 +0300

    feat: add the background change over mouse hover with opacity darkening after multiple passes

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
 
 
 const canvas = document.querySelector(".canvas");
+const numOfDivs = 16*16;
 
-const numOfDivs = 256;
 
 for(let i = 0 ; i < numOfDivs ; i++ ){
 
     let Div = document.createElement("div");
     Div.classList.add("grid")
     canvas.appendChild(Div);
+}
+
+
+const cells = document.querySelectorAll(".grid");
+
+cells.forEach(cell => { 
+    
+    cell.addEventListener("mouseover", () => setBGColor(cell) )
+    
+    
+});
+
+
+
+
+
+function setBGColor(cell){
+
+    cell.style.backgroundColor = "black";
+
+    let currentOpacity = Number(cell.style.opacity);
+
+    if(currentOpacity < 1){
+
+        cell.style.opacity = `${currentOpacity + 0.25}`; 
+
+    }
+    console.log(currentOpacity);
+    
 }
