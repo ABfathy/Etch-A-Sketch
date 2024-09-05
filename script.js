12881a085949355f25f02dc5140ddfa86fe4741d
ABfathy <113472069+ABfathy@users.noreply.github.com>
2024-09-05 16:47:49 +0300

    feat: add the eraser toggle function and some logic for the clear function

--- script.js
+++ script.js
@@ -1,20 +1,81 @@
 
 
 const canvas = document.querySelector(".canvas");
+let color = "black";
+let numOfDivs = 0;
 
-const numOfDivs = prompt("choose the number of divs")
 
-const buttons = document.querySelectorAll("li")
+const buttons = document.querySelectorAll("li");
+const eraser = document.querySelector(".eraser");
+const rainbow = document.querySelector(".rainbow");
+const clear = document.querySelector(".clear");
+const size = document.querySelector(".size");
+
+
+
+
+function marker(){
+
+    const cells = document.querySelectorAll(".gridRow");
+
+    cells.forEach(cell => { 
+        
+        cell.addEventListener("mouseover", () => setBGColor( cell , color ) )
+        
+    
+});}
+
+
+
+function chosenButton (button){
+
+    if (button === size ){
+
+       numOfDivs = prompt("Choose a Grid size! (0 -> 100) ");
+
+       canvasCreator(numOfDivs);
+       marker();
+       
+    }
+    
+    if (button === eraser && buttonChecker(button)){
+        color = "rgb(217, 250, 250)";
+        button.classList.add("on")
+    }
+
+    if (button === clear){
+        
+        const cells = document.querySelectorAll(".gridRow");
+        cells.forEach(cell => {cell.style.backgroundColor = "rgb(217, 250, 250)";})
+
+    }
+
+}
+
+
+function buttonChecker(button){
+
+    if (button.classList.contains("on")){
+        color = "black";
+        button.classList.remove("on");
+        return false;
+
+    } else 
+    
+    return true;
+}
+
 
 
 buttons.forEach( button  => {
 
     button.addEventListener("click" , () => clickAnimation(button));
-    button.addEventListener("transitionend" , () => resetClickAnimation(button) )
-
+    button.addEventListener("transitionend" , () => resetClickAnimation(button));
+    button.addEventListener("click" , () =>chosenButton(button));
 }) 
 
 
+function canvasCreator(numOfDivs){
 
 for(let i = 0 ; i < numOfDivs ; i++ ){
 
@@ -32,27 +93,20 @@ for(let i = 0 ; i < numOfDivs ; i++ ){
     canvas.appendChild(column);
 }
 
+}
 
 
-const cells = document.querySelectorAll(".gridRow");
-
-cells.forEach(cell => { 
-    
-    cell.addEventListener("mouseover", () => setBGColor(cell) )
-    
-    
-});
 
 
 
 function resetClickAnimation(button){
 
     button.classList.remove("clicked");
-    
+
 }
+
 function clickAnimation(button){
 
-    console.log("button function")
     button.classList.add("clicked");
 
 
@@ -60,9 +114,9 @@ function clickAnimation(button){
 }
 
 
-function setBGColor(cell){
+function setBGColor(cell , color){
 
-    cell.style.backgroundColor = "black";
+    cell.style.backgroundColor = color;
 
     let currentOpacity = Number(cell.style.opacity);
 
